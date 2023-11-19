b45106b58c6d972c47bee8216dade0d8c9f9ff89
Majki1 <mikulec.marin52@gmail.com>
2023-11-19 21:42:21 +0100

    login screen

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -8,11 +8,32 @@ export class Home extends Component {
   render() {
     return (
 <div class="hero min-h-screen bg-base-100">
-  <div class="hero-content text-center">
-    <div class="max-w-md">
-      <h1 class="text-5xl font-bold text-white">Dobro došli</h1>
-      <p class="py-6 text-white">Random text zamjenit cu kasnije</p>
-      <Link className="btn bg-slate-500 text-white" to="Tickets">Podigni ticket</Link>
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <div class="text-center lg:text-left">
+      <h1 class="text-5xl font-bold text-white">Login now!</h1>
+      <p class="py-6 text-white">Random text mrnjaaaaaaaau</p>
+    </div>
+    <div class="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <form class="card-body">
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text text-black">Email</span>
+          </label>
+          <input type="email" placeholder="email" class="input input-bordered" required />
+        </div>
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text text-black">Password</span>
+          </label>
+          <input type="password" placeholder="password" class="input input-bordered" required />
+          <label class="label">
+            <a href="#" class="label-text-alt link link-hover text-black">Forgot password?</a>
+          </label>
+        </div>
+        <div class="mt-6 bg-base-100">
+          <button class="btn bg-slate-600 text-white">Login</button>
+        </div>
+      </form>
     </div>
   </div>
 </div>
